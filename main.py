ae387a980f300c4e89a14e41ebb22f2597769156
lanesky <shangkeyun@gmail.com>
2023-06-20 11:52:01 +0900

    bugfix: wrong lib for parser

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ async def fetch_trial_data(trialID):
     return response.text
 
 def parse_eligibility_criteria(html):
-    soup = BeautifulSoup(html, 'html.parser')
+    soup = BeautifulSoup(xml, 'html.parser')
     eligibilityCriteria = soup.find(id="eligibility").parent.parent.parent.find_all('ul')
     print(eligibilityCriteria)
     inclusionCriteria, exclusionCriteria = eligibilityCriteria[0].text.strip(), eligibilityCriteria[1].text.strip()
